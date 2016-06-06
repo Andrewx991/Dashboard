f0f70b6992b4b6f8423555626bb52dffc60f1342
Jeremy Rosenberg <jerrosenberg@yahoo.com>
2016-06-05 22:30:04 -0400

    Add end-to-end traffic call, load api key from file, updated readme.

--- src/Services/traffic.js
+++ src/Services/traffic.js
@@ -1,24 +1,36 @@
+var fileSystem = require('fs');
+var path = require('path');
 var querystring = require('querystring');
 var request = require('request');
 
 module.exports = {
   getTrafficData: function(options, callback) {
-    var escapedApiKey = querystring.escape(options.apiKey);
-    var escapedFrom = querystring.escape(options.from);
-    var escapedTo = querystring.escape(options.to);
+    var apiKeyDirectory = path.join(__dirname, '..', '..', 'apikeys');
+    var forecastApiKeyFullPath = path.join(apiKeyDirectory, 'mapquest');
 
-    var path = 'http://www.mapquestapi.com/directions/v2/route?key=' + escapedApiKey + '&from=' + escapedFrom + '&to=' + escapedTo;
-
-    request(path, function(err, response, body){
+    fileSystem.readFile(forecastApiKeyFullPath, {encoding: 'utf-8'}, function(err, apiKey){
       if (err) {
-        callback(err);
+        callback('Error loading forecast API key: ' + err);
         return;
       }
 
-      body = JSON.parse(body);
+      var escapedApiKey = querystring.escape(apiKey.trim());
+      var escapedFrom = querystring.escape(options.from);
+      var escapedTo = querystring.escape(options.to);
 
-      callback(null, {
-        timeInSeconds: body.route.realTime
+      var path = 'http://www.mapquestapi.com/directions/v2/route?key=' + escapedApiKey + '&from=' + escapedFrom + '&to=' + escapedTo;
+
+      request(path, function(err, response, body){
+        if (err) {
+          callback('Error requesting traffic data: ' + err);
+          return;
+        }
+
+        body = JSON.parse(body);
+
+        callback(null, {
+          timeInSeconds: body.route.realTime
+        });
       });
     });
   }
